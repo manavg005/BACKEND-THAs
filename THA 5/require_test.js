02d5df9d4c985720204e9be87f2d237705b82526
manavg005 <manavguptamg1990@gmail.com>
2021-09-14 19:49:25 +0530

    Day 5 and 6

---
+++ THA 5/require_test.js
@@ -0,0 +1,14 @@
+class Test{
+    print() {
+        console.log("I am inside require_test");
+    }
+}
+
+class Test2{
+     print(){
+         console.log(">>>>>");
+     }
+}
+
+module.exports= Test2;
+
